cb6aa1e5c5be730984abc69f090d1f47582c1451
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-07-05 09:22:53 -0700

    Cleaning up Jsonnet Parser logic, adding comments

--- src/main/java/cd/go/plugin/config/yaml/JsonnetConfigParser.java
+++ src/main/java/cd/go/plugin/config/yaml/JsonnetConfigParser.java
@@ -6,71 +6,119 @@ import java.io.InputStream;
 
 import cd.go.plugin.config.yaml.transforms.RootTransform;
 
+/**
+ * Extends {@link YamlConfigParser} to parse jsonnet files into a JsonConfigCollection.
+ */
 public class JsonnetConfigParser extends YamlConfigParser {
+
+    /**
+     * Create a new JsonnetConfigParser.
+     */
     public JsonnetConfigParser() {
         super(new RootTransform());
     }
 
+    /**
+     * Parse a list of jsonnet files into a JsonConfigCollection.
+     * 
+     * @param baseDir The base directory to resolve relative paths against
+     * @param files The list of jsonnet files to parse
+     * @return The JsonConfigCollection containing the parsed config
+     */
     @Override
     public JsonConfigCollection parseFiles(File baseDir, String[] files) {
         JsonConfigCollection collection = new JsonConfigCollection();
         for (String file : files) {
+            InputStream input = null;
             try {
                 File filePath = new File(baseDir, file);
-                InputStream input = compileJsonnet(filePath.toString());
+                input = compileJsonnet(filePath.toString());
                 // Calling YamlConfigParser's parseStream method (instead of the overridden one below)
                 super.parseStream(collection, input, file);
             } catch (NullPointerException e) {
                 collection.addError("File matching GoCD Jsonnet pattern disappeared", file);
+            } catch (JsonnetEvalException e) {
+                collection.addError(e.getMessage(), file);
+            } finally {
+                // Close the input stream if it was opened
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        collection.addError("Error while closing Jsonnet file", file);
+                    }
+                }
             }
         }
         return collection;   
     }
-
+    
+    /** 
+     * Parse a stream of jsonnet into a JsonConfigCollection.
+     * 
+     * @param result The JsonConfigCollection to add the parsed config to
+     * @param input The InputStream containing the jsonnet to parse
+     * @param location The location of the jsonnet file
+     */
     @Override
     public void parseStream(JsonConfigCollection result, InputStream input, String location) {
+        InputStream jsonInputStream = null;
         try {
-            InputStream jsonInputStream = compileJsonnet(input);
+            jsonInputStream = compileJsonnet(input);
             super.parseStream(result, jsonInputStream, location);
         } catch (NullPointerException ex) {
             result.addError("File matching GoCD Jsonnet pattern disappeared", location);
+        } catch (IOException ex) {
+            result.addError("Error while reading Jsonnet file", location);
+        } catch (JsonnetEvalException ex) {
+            result.addError(ex.getMessage(), location);
+        } finally {
+            // Close the input stream if it was opened
+            if (jsonInputStream != null) {
+                try {
+                    jsonInputStream.close();
+                } catch (IOException ex) {
+                    result.addError("Error while closing Jsonnet file", location);
+                }
+            }
         }
     }
 
-    private InputStream compileJsonnet(String filePath) {
-        InputStream jsonInputStream = null;
-        try {
-            ProcessBuilder pb = new ProcessBuilder("jrsonnet", filePath, "--format", "yaml");
-            Process p = pb.start();
-            int exitCode = p.waitFor();
-            if (exitCode != 0) {
-                System.out.println("Error compiling jsonnet file: " + filePath);
-            }
-            jsonInputStream = p.getInputStream();
-        } catch (IOException e) {
-            System.out.println("Error compiling jsonnet file: " + e.getMessage());
-        } catch (InterruptedException e) {
-            System.out.println("Error compiling jsonnet file: " + e.getMessage());
-        }
-        return jsonInputStream;
+    /**
+     * Compile jsonnet using the jrsonnet command line tool.
+     * 
+     * @param input InputStream containing the jsonnet to compile
+     * @return InputStream containing the compiled jsonnet
+     * @throws IOException if there is an error reading the input stream
+     * @throws JsonnetEvalException if jrsonnet exits with an error
+     */
+    private InputStream compileJsonnet(InputStream input) throws IOException, JsonnetEvalException {
+        String inputString = new String(input.readAllBytes());
+        return compileJsonnet("--exec", inputString);
     }
 
-    private InputStream compileJsonnet(InputStream input) {
-        InputStream jsonInputStream = null;
+    
+    /** 
+     * Compile jsonnet using the jrsonnet command line tool.
+     * 
+     * @param command The command to pass to jrsonnet
+     * @return InputStream containing the compiled jsonnet
+     * @throws JsonnetEvalException if jrsonnet exits with an error
+     */
+    private InputStream compileJsonnet(String... command) throws JsonnetEvalException {
+        String[] commandWithArgs = new String[command.length + 1];
+        commandWithArgs[0] = "jrsonnet";
+        System.arraycopy(command, 0, commandWithArgs, 1, command.length);
         try {
-            String inputString = new String(input.readAllBytes());
-            ProcessBuilder pb = new ProcessBuilder("jrsonnet", "--exec", inputString, "--format", "yaml");
+            ProcessBuilder pb = new ProcessBuilder(commandWithArgs);
             Process p = pb.start();
             int exitCode = p.waitFor();
             if (exitCode != 0) {
-                System.out.println("Error compiling jsonnet: " + inputString);
+                throw new Exception("Jsonnet exited with an error");
             }
-            jsonInputStream = p.getInputStream();
-        } catch (IOException e) {
-            System.out.println("Error compiling jsonnet file: " + e.getMessage());
-        } catch (InterruptedException e) {
-            System.out.println("Error compiling jsonnet file: " + e.getMessage());
+            return p.getInputStream();
+        } catch (Exception e) {
+            throw new JsonnetEvalException("Error while evaluating jsonnet: " + e.getMessage());
         }
-        return jsonInputStream;
     }
 }
---
+++ src/main/java/cd/go/plugin/config/yaml/JsonnetEvalException.java
@@ -0,0 +1,7 @@
+package cd.go.plugin.config.yaml;
+
+public class JsonnetEvalException extends RuntimeException {
+    public JsonnetEvalException(String message) {
+        super(message);
+    }
+}
