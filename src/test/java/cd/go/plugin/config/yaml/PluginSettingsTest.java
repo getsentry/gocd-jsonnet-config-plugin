ae3f048cf6b7089143bd16d41227a3cc59bae0dd
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-07-26 15:29:15 -0700

    Adding jsonnet flags setting

--- src/test/java/cd/go/plugin/config/yaml/PluginSettingsTest.java
+++ src/test/java/cd/go/plugin/config/yaml/PluginSettingsTest.java
@@ -9,23 +9,55 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class PluginSettingsTest {
     @Test
     public void shouldGetFilePattern() {
-        PluginSettings pluginSettings = new PluginSettings("file-pattern", null);
+        PluginSettings pluginSettings = new PluginSettings("file-pattern", null, null);
 
         assertThat(pluginSettings.getFilePattern(), is(equalTo("file-pattern")));
     }
 
     @Test
     public void shouldGetJsonnetCommand() {
-        PluginSettings pluginSettings = new PluginSettings(null, "jsonnet-command");
+        PluginSettings pluginSettings = new PluginSettings(null, "jsonnet-command", null);
 
         assertThat(pluginSettings.getJsonnetCommand(), is(equalTo("jsonnet-command")));
     }
 
     @Test
+    public void shouldGetJsonnetFlags() {
+        PluginSettings pluginSettings = new PluginSettings(null, null, "jsonnet-flags");
+
+        assertThat(pluginSettings.getJsonnetFlags(), is(equalTo("jsonnet-flags")));
+    }
+
+    @Test
     public void shouldGetFilePatternAndJsonnetCommand() {
-        PluginSettings pluginSettings = new PluginSettings("file-pattern", "jsonnet-command");
+        PluginSettings pluginSettings = new PluginSettings("file-pattern", "jsonnet-command", null);
 
         assertThat(pluginSettings.getFilePattern(), is(equalTo("file-pattern")));
         assertThat(pluginSettings.getJsonnetCommand(), is(equalTo("jsonnet-command")));
+    }
+
+    @Test
+    public void shouldGetFilePatternAndJsonnetFlags() {
+        PluginSettings pluginSettings = new PluginSettings("file-pattern", null, "jsonnet-flags");
+
+        assertThat(pluginSettings.getFilePattern(), is(equalTo("file-pattern")));
+        assertThat(pluginSettings.getJsonnetFlags(), is(equalTo("jsonnet-flags")));
+    }
+
+    @Test
+    public void shouldGetJsonnetCommandAndJsonnetFlags() {
+        PluginSettings pluginSettings = new PluginSettings(null, "jsonnet-command", "jsonnet-flags");
+
+        assertThat(pluginSettings.getJsonnetCommand(), is(equalTo("jsonnet-command")));
+        assertThat(pluginSettings.getJsonnetFlags(), is(equalTo("jsonnet-flags")));
+    }
+
+    @Test
+    public void shouldGetFilePatternAndJsonnetCommandAndJsonnetFlags() {
+        PluginSettings pluginSettings = new PluginSettings("file-pattern", "jsonnet-command", "jsonnet-flags");
+
+        assertThat(pluginSettings.getFilePattern(), is(equalTo("file-pattern")));
+        assertThat(pluginSettings.getJsonnetCommand(), is(equalTo("jsonnet-command")));
+        assertThat(pluginSettings.getJsonnetFlags(), is(equalTo("jsonnet-flags")));
     }
 }
